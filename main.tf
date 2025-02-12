ea7b8256916e98b96a556c649588ec40a7d9066d
Raghu <raghu.esh@gmail.com>
2025-02-12 15:56:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "opus_viewer_bindings" {
-  project_context = module.workload_context.project_context
+  project_context = module.workload_context.project_context1
   member          = "xyz"
   roles = [
     "roles/viewer",
